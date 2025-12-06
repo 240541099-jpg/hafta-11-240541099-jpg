32db6260d887348df793f696152dc070b9d76800
Zübeyir <240541099@firat.edu.tr>
2025-12-06 14:47:51 +0300

    Implement printAmerican method for date formatting

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,7 +1,8 @@
 public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+      public static void printAmerican(String day,int date,String month,int year){
+    System.out.println(day+"/"+date+"/"+month+"/"+year);
+        
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
@@ -10,5 +11,6 @@ public class Exercise4_1 {
 
     public static void main(String[] args) {
         // TODO: test methods
+                printAmerican("monday",2,"july",2999);
     }
 }
